8d6c713e25d97cc8f482833c92c3297a5d515986
spooler <paul@spooler.org>
2010-09-30 20:07:28 -0300

    Fixed an error on some views

---
+++ db/migrate/20100929205412_add_completed_to_tasks.rb
@@ -0,0 +1,9 @@
+class AddCompletedToTasks < ActiveRecord::Migration
+  def self.up
+    add_column :tasks, :completed_at, :date
+  end
+
+  def self.down
+    remove_column :tasks, :completed_at
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100922193853) do
+ActiveRecord::Schema.define(:version => 20100929205412) do
 
   create_table "lists", :force => true do |t|
     t.string   "name"
@@ -22,9 +22,10 @@ ActiveRecord::Schema.define(:version => 20100922193853) do
   create_table "tasks", :force => true do |t|
     t.string   "description"
     t.integer  "list_id"
-    t.boolean  "done",        :default => false
+    t.boolean  "done",         :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "completed_at"
   end
 
   create_table "users", :force => true do |t|
